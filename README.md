18621697de93bddaf9e971e7803007784eef062e
danielmoi <daniel@ampersandmoi.com>
2018-01-04 13:02:00 +1100

    Allow logger, add null-logger, update docs

--- README.md
+++ README.md
@@ -16,3 +16,23 @@ async function failure(e, attempt) {
 const runner = retry({ max: 5, delay: 1000, factor: 2 }, failure);
 await runner(run, 1);
 ```
+
+---
+
+Optionally pass in a logger:
+
+```javascript
+const { retry } = require('retry-machine');
+const logger = require('./logger');
+
+async function run(count) {
+  console.log(count);
+}
+
+async function failure(e, attempt) {
+  console.error(e);
+}
+
+const runner = retry({ max: 5, delay: 1000, factor: 2, logger }, failure);
+await runner(run, 1);
+```
--- src/index.js
+++ src/index.js
@@ -1,23 +1,35 @@
-// @flow
-const wait = (ms: number) => new Promise(resolve => setTimeout(() => { resolve(); }, ms));
+const nullLogger = require('null-logger');
 
-const backoff = (delay: number, factor: number, attempt: number) => (
-  Math.round(delay * (factor ** attempt))
-);
+// @flow
+const wait = (ms: number) =>
+  new Promise(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, ms),
+  );
+
+const backoff = (delay: number, factor: number, attempt: number) =>
+  Math.round(delay * factor ** attempt);
 
 type Config = {
   max?: number,
   delay?: number,
   factor?: number,
-}
+  logger?: mixed => void,
+};
 
-function retry({ max = 3, delay = 1000, factor = 1 }: Config = {}, failure: Function = async () => {}): Function {
-  return async function (promise, ...args) {
+function retry(
+  { max = 3, delay = 1000, factor = 1, logger = nullLogger }: Config = {},
+  failure: Function = async () => {},
+): Function {
+  return async function closure(promise, ...args) {
     let attempts = max || 3;
     let result = null;
 
+    /* eslint-disable no-await-in-loop */
     while (attempts > 0) {
       try {
+        logger.info('Retry Machine: ', ...args);
         result = await promise(...args);
         break;
       } catch (e) {
